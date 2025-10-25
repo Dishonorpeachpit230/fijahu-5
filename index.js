b0405dcca4b314fb22686016c07d2e24b6b31288
zemguimaraes <josemaria_vasconcelosguimaraes@hotmail.com>
2025-10-25 16:34:12 +0000

    Initial commit: javascript project

---
+++ index.js
@@ -0,0 +1,16 @@
+/**
+ * fijahu-5 - JavaScript Implementation
+ * A simple Hello World program
+ */
+
+function main() {
+    console.log("Hello, World!");
+    console.log("Welcome to fijahu-5");
+    
+    // Simple calculation
+    const numbers = [1, 2, 3, 4, 5];
+    const total = numbers.reduce((sum, num) => sum + num, 0);
+    console.log(`Sum of numbers: ${total}`);
+}
+
+main();
